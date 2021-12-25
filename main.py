b4e46720386470f38382d25cd8e27e076917f176
JuanMayorga <juank-963@hotmail.com>
2021-12-24 19:53:51 -0500

    started game-logic

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from models.player import Player
+from models.question import Question
+from models.level import Level
 from DIR.constant import *
 import random
 
 import os
 import json
 
+
 def clear():
     if os.name == "nt":
         os.system("cls")
     else:
         os.system("clear")
+
 
 def validate(op):
     try:
@@ -22,27 +26,81 @@ def validate(op):
             if res.upper() != "Y" and res.upper() != "N":
                 print(res.upper() != "N")
                 raise
+        if op == 3:
+            res = input("\nWrite the correct answer and then press Enter.\n")
+            res = res.upper()
+            if res != "B" and res != "A" and res != "C" and res != "D":
+                raise
+            if res == "A":
+                res = 0
+            if res == "B":
+                res = 1
+            if res == "C":
+                res = 2
+            if res == "D":
+                res = 3
         return res
     except:
         input("Select a valid option.")
 
+
 def load_questions():
+    questions = []
     try:
-        with open(DATA, "r", encoding = "utf-8") as f:
+        with open(DATA, "r", encoding="utf-8") as f:
             data = f.read()
             jsondata = json.loads(data)
-        for line in range(len(jsondata)):
-            txt = f"{line + 1}. " + jsondata[line]["name"]
-            print(txt.ljust(20, "."), end=" ")
-            print(jsondata[line]["score"])
-        input("\n\nPress Enter to continue....")
+        for line in jsondata:
+            question_obj = Question(line["statement"], line["op1"], line["op2"],
+                                    line["op3"], line["op4"], line["answer"], line["difficulty"])
+            question_obj.set_name = line["category"]
+            questions.append(question_obj)
         f.close()
+        return questions
     except:
-        print("\n\nLeaderboards not fount, please play a game first")
+        print("\n\nData questions not found, please put a data before start game")
         input("\nPress Enter to continue....")
+
+
+def game():
+    questions = load_questions()
+    lvls = []
+    score = 0
+    cat_num = len(set(i.get_name for i in questions))
+
+    for i in range(0, cat_num):
+        lvls.append(Level(i+1, (i+1)*500))
+    for j in lvls:
+        clear()
+        res = None
+        question_group = [
+            x for x in questions if j.get_level_number == x.get_difficulty]
+        selected_question = question_group[random.randint(
+            0, len(question_group) - 1)]
+        options = [selected_question.get_op1, selected_question.get_op2,
+                   selected_question.get_op3, selected_question.get_op4]
+        random.shuffle(options, random.random)
+        while res == None:
+            print(f"QUESTION {j.get_level_number}".center(50, "="), end=" ")
+            print(f"score: {score} \n")
+            print(selected_question.get_statement)
+            print(f"a) {options[0]}")
+            print(f"b) {options[1]}")
+            print(f"c) {options[2]}")
+            print(f"d) {options[3]}")
+            res = validate(3)
+        if options[res] == selected_question.get_answer:
+            print("¡¡¡¡RESPUESTA CORRECTA!!!!!")
+            score += j.get_points
+        else:
+            print("RESPUESTA INCORRECTA")
+        input("\n\nPres Enter to show next question.")
+
+
 
 def show_scores():
     pass
+
 
 def run():
     option = 0
@@ -54,13 +112,14 @@ def run():
         print("3. Exit.\n")
         option = validate(1)
         if option == 1:
-            pass
-            #Comienza un nuevo juego
+            game()
+            # Comienza un nuevo juego
         if option == 2:
             show_scores()
         if option == 3:
             clear()
             return "Thanks for playing"
+
 
 if __name__ == '__main__':
     run()
--- models/question.py
+++ models/question.py
@@ -1,58 +1,61 @@
-class Question:
-    def __init__(self, statement, op1, op2, op3, op4, answer, difficulty):
-        self.statement = statement
-        self.op1 = op1
-        self.op2 = op2
-        self.op3 = op3
-        self.op4 = op4
-        self.answer = answer
-        self.difficulty = difficulty
+from models.category import Category
 
-    @property.getter
+class Question(Category):
+    def __init__(self, statement, op1, op2, op3, op4, answer, difficulty):
+        self._statement = statement
+        self._op1 = op1
+        self._op2 = op2
+        self._op3 = op3
+        self._op4 = op4
+        self._answer = answer
+        self._difficulty = difficulty
+        super().__init__(name =  "category")
+
+    @property
     def get_statement(self):
         return self._statement
-    @property.setter
+    @get_statement.setter
     def set_statement(self, statement):
        self._statement = statement
 
-    @property.getter
+    @property
     def get_op1(self):
         return self._op1
-    @property.setter
+    @get_op1.setter
     def set_op1(self, op1):
        self._op1 = op1
 
-    @property.getter
+    @property
     def get_op2(self):
         return self._op2
-    @property.setter
+    @get_op2.setter
     def set_op2(self, op2):
        self._op2 = op2
 
-    @property.getter
+    @property
     def get_op3(self):
         return self._op3
-    @property.setter
+    @get_op3.setter
     def set_op3(self, op3):
        self._op3 = op3
 
-    @property.getter
+    @property
     def get_op4(self):
         return self._op4
-    @property.setter
+    @get_op4.setter
     def set_op4(self, op4):
        self._op4 = op4
 
-    @property.getter
+    @property
     def get_answer(self):
         return self._answer
-    @property.setter
+    @get_answer.setter
     def set_answer(self, answer):
        self._answer = answer
 
-    @property.getter
+    @property
     def get_difficulty(self):
         return self._difficulty
-    @property.setter
+    @get_difficulty.setter
     def set_difficulty(self, difficulty):
        self._difficulty = difficulty
